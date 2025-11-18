f5b443763401c9b24d28f96eec33b705724c25be
Sarva-gms <sarva@gmobility.com>
2025-11-18 13:36:15 +0530

    now , i learn how to open a product page by using flask

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/product')
+def product():
+    return 'this is product page'
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5100)
